6d3fbc71d4cde4d3df3affb90f7bdd4fcdad0e52
Makyra <makyrakim@utexas.edu>
2022-08-03 12:05:17 -0500

    [fix] model 에서 name 수정

--- users/models.py
+++ users/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    name = models.CharField(max_length=50, default="not specified", verbose_name="이름")
+    # TODO: null=True 이면 안됨! 아이디찾을때 필요.
+    name = models.CharField(max_length=50, null=True, verbose_name="이름")
     email = models.CharField(max_length=100, verbose_name="이메일")
     nickname = models.CharField(null=True, max_length=50, verbose_name="닉네임") #default=username 방법???
     
