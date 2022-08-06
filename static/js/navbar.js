275b7902be8658f9be247cee9badb465e5a0397f
parkyejin <uiop5809@naver.com>
2022-08-06 19:58:47 +0900

    [design] navbar UI 수정

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -2,6 +2,5 @@ const navbarBtn = document.querySelector(".main__navbar-btn img");
 const navbarList = document.querySelector(".main__navbar-list");
 
 navbarBtn.addEventListener("click", function () {
-  navbarBtn.classList.toggle("hidden");
   navbarList.classList.toggle("hidden");
 });
