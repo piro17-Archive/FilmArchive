9c014c9aea79e110043f6356fea181989eff01b8
Makyra <makyrakim@utexas.edu>
2022-08-07 11:20:35 -0500

    [feat] 비밀번호 이메일로 리셋 1차

--- config/settings.py
+++ config/settings.py
@@ -164,8 +164,6 @@ LOGIN_REDIRECT_URL = '/'
 #여기부터추가
 ACCOUNT_EMAIL_VERIFICATION = "none"
 
-# LOGIN_REDIRECT_URL = "/"
-
 ACCOUNT_LOGOUT_REDIRECT_URL = "/"
 
 ACCOUNT_LOGOUT_ON_GET = True
@@ -183,4 +181,13 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+
+#RESET PASSWORD
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# 이건 내 정보여서 숨김처리했음!
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
