422e57631957ebe61c05b33f1fc46a21aee7bafa
parkyejin <uiop5809@naver.com>
2022-08-08 14:53:55 +0900

    [design] navbar btn 클릭시 디자인 변경

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,6 +1,8 @@
 const navbarBtn = document.querySelector(".main__navbar-btn img");
 const navbarList = document.querySelector(".main__navbar-list");
+const background = document.querySelector("main");
 
 navbarBtn.addEventListener("click", function () {
   navbarList.classList.toggle("hidden");
+  background.classList.toggle("overlay");
 });
