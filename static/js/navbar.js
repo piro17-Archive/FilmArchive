4b033a1c6b19b60800e9c4a713f768af8f9b0ddd
parkyejin <uiop5809@naver.com>
2022-08-16 20:52:39 +0900

    [feature] navbar scroll 반응형 구현

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -6,3 +6,14 @@ navbarBtn.addEventListener("click", function () {
   navbarList.classList.toggle("hidden");
   body.classList.toggle("overlay");
 });
+
+const navbar = document.querySelector(".main__navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+window.addEventListener("scroll", () => {
+  if(window.scrollY > navbarHeight) {
+    navbar.setAttribute("style", "background: var(--main); transition: 0.4s;");
+  } else {
+    navbar.setAttribute("style", "background: transparent; transition: 0.4s;");
+  }
+})
