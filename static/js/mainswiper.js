87324102400924cfee0da1ccec34eef76e90a803
parkyejin <uiop5809@naver.com>
2022-08-21 17:05:05 +0900

    [feature] swiper autoplay

--- static/js/mainswiper.js
+++ static/js/mainswiper.js
@@ -1,16 +1,22 @@
 const swiper = new Swiper('.swiper', {
-  direction: 'vertical',
-  loop: true,
+  slidesPerView: 1, //레이아웃 뷰 개수
 
-  // autoplay: {
-  //   delay: 3000,
-  //   disableOnInteraction: false,
-  // },
-
-  pagination: {
-    el: '.swiper-pagination',
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
   },
 
+  // Optional parameters
+  direction: 'horizontal', 
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable : true,
+  },
+
+  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
